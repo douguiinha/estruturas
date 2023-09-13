8afd13405d11d2f72b78e45edb43f06aa722c084
douguiinha <142998950+douguiinha@users.noreply.github.com>
2023-09-13 09:56:46 -0300

    Add files via upload

---
+++ repetiçao.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+	int numero;
+	int contador;
+
+	printf("Digite um numero Inteiro Positivo: ");
+	fflush(stdout);//Necessário para Bug do eclipse em Windows
+	scanf("%d", &numero);
+
+	if(numero > 0){
+		printf("\nNumeros Naturais de 0 ate %d:\n\n", numero);
+
+			for(contador = 0; contador <= numero; contador ++){
+				printf("%d ", contador);
+			}
+	}else{
+		printf("\nNumero Invalido!!!");
+	}
+
+	return 0;
+}
